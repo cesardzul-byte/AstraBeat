f3444ea0e385799ea581e93e8b0d15c3b7451ecb
CesarDzl <cesardzul411@gmail.com>
2026-07-26 16:58:10 -0600

    feat: Fase 0.3. Se agrega la nevagción entre pantallas mediante 'Link' (Completado)

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
 // Pantalla principal: biblioteca de canciones del dispositivo
 import { View, Text } from 'react-native';
 
+// Link representa navegación declarativa: Se declara el destino mediante 'href' y Expo Router se encarga de cambiar de pantalla sin tener
+// que llamar manualmente a una función. 
+// Los usuarios pueden hacer clic en él para navegar a otra pantalla.
+import { Link } from 'expo-router'; 
+
 export default function Library() {
     return (
         <View>
             <Text>Contenido de la biblioteca de musica</Text>
+
+            <Link href="/player">Abrir el reproductor</Link> 
+
+            <Link href="/stats">Ver estadísticas</Link>
         </View>
     ); 
 }
