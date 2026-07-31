1333cbdcedb27987047b4f96379fa338c15ce443
CesarDzl <cesardzul411@gmail.com>
2026-07-31 10:58:18 -0600

    feat (navigation): se configuran encabezados y tema oscuro del stack (fase 0.6)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,50 @@
 // Layout raíz de navegación de la aplicación. 
 import { Stack } from 'expo-router'; 
 
+// Se realiza la importanción correspondiente del objeto de tokens 
+import { colors} from '../src/theme/tokens';
+
+/**
+ * Layout raíz de AstraBEat
+ * 
+ * Define la configuración visual compartida del encabezado 
+ * y registra las pantallas principales de la aplicación
+ */
 export default function RootLayout() {
-    return <Stack/>
+    return (
+        <Stack
+        screenOptions={{
+            //Define el color de fondo del encabezado
+            headerStyle: {
+                backgroundColor: colors.surface,
+            },
+
+            // Define el color del título y del botón para regresar
+            headerTintColor: colors.text,
+
+            // Define el fondo del área donde aparece cada pantalla
+            contentStyle: {
+                backgroundColor: colors.background,
+            }, 
+        }}
+        >
+        {/* Configuración de la pantalla app/index.tsx */}
+        <Stack.Screen
+            name="index"
+            options={{ title: 'Biblioteca' }}
+        />
+
+        {/* Configuración de la pantalla app/player.tsx */}
+        <Stack.Screen
+            name="player"
+            options={{ title: 'Reproductor' }}
+        />
+
+        {/* Configuración de la pantalla app/stats.tsx */}
+        <Stack.Screen
+            name="stats"
+            options={{title: 'Estadísticas'}}
+        />
+        </Stack>
+    ); 
 }
-
-
-
